3cde9f8a68ef93ea5e05263e258f85cb18992dc6
Nikhil Bailey <nikhil.bailey@colorado.edu>
2023-11-06 12:03:34 -0700

    added database configurations to index.js

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const pgp = require('pg-promise')()
 // start the server
 app.listen(3000)
 console.log('Server is listening on port 3000')
+
+// database configuration
+const dbConfig = {
+    host: 'db',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+}
+const db = pgp(dbConfig)
