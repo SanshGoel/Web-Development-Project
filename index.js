8ef1bca228c6fec38a2667cd9194094fbba2326c
Nikhil Bailey <nikhil.bailey@colorado.edu>
2023-11-08 14:59:18 -0700

    added authentication and route templates to index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // import dependencies
-const express = require('express'); // To build an application server or API
-const app = express();
+const express = require('express') // To build an application server or API
+const app = express()
 const pgp = require('pg-promise')() // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser')
-const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const session = require('express-session') // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcrypt') //  To hash passwords
 const axios = require('axios') // To make HTTP requests from our server. We'll learn more about it in Part B.
-
-
-// start the server
-app.listen(3000)
-console.log('Server is listening on port 3000')
 
 // database configuration
 const dbConfig = {
@@ -23,8 +18,8 @@ const dbConfig = {
 const db = pgp(dbConfig)
 
 // App settings
-app.set('view engine', 'ejs'); // set the view engine to EJS
-app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+app.set('view engine', 'ejs') // set the view engine to EJS
+app.use(bodyParser.json()) // specify the usage of JSON for parsing request body.
 
 // initialize session variables
 app.use(
@@ -33,10 +28,54 @@ app.use(
         saveUninitialized: false,
         resave: false,
     })
-);
+)
 
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
-);
+)
+
+// *****************************************************
+//                  API Routes
+// *****************************************************
+
+// *****************************************************
+//                  Without Auth
+// *****************************************************
+
+app.get('/', (req, res) => {
+    res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+    res.render('pages/login',{})
+})
+
+// *****************************************************
+//                 With Auth
+// *****************************************************
+
+// Authentication Middleware.
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+        // Default to login page.
+        return res.redirect('/login')
+    }
+    next()
+}
+
+// Authentication Required
+app.use(auth)
+
+app.get('/home', (req, res) => {
+
+})
+
+app.get("/logout", (req, res) => {
+
+})
+
+// start the server
+app.listen(3000)
+console.log('Server is listening on port 3000')
