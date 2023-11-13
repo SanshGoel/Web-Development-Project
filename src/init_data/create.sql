93c5ae2c50363604ad20d5d3872e09acaa17bcc6
Vidhaan Singhvi <visi5704@colorado.edu>
2023-11-13 13:37:21 -0700

    updated again

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,9 +1,20 @@
 CREATE TABLE users (
-    user_id INT PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(60) NOT NULL,
     display_name VARCHAR(100),
     phone VARCHAR(20),
     email VARCHAR(100)
 );
+
+CREATE TABLE friends (
+    user_id_1 INT,
+    user_id_2 INT,
+    FOREIGN KEY (user_id_1) REFERENCES users(user_id),
+    FOREIGN KEY (user_id_2) REFERENCES users(user_id),
+    PRIMARY KEY (user_id_1, user_id_2)
+);
+
+
+
 
