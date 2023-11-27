a73dd146d057e12e2889d3a99ad3240047501a77
Sansh28 <sago1198@colorado.edu>
2023-11-27 14:24:41 -0700

    added testcases

--- src/test/server.spec.js
+++ src/test/server.spec.js
@@ -26,3 +26,79 @@ describe('Server!', () => {
     // ===========================================================================
     // TO-DO: Part A Login unit test case
 })
+
+//Positive cases
+it('positive : /login', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({username: 'user123', password: 'password'})
+      .end((err, res) => {
+        //expect(res.body).to.have.property('accessToken');
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+//We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 400 along with a "Invalid input" message.
+it('Negative : /login. Checking invalid name', done => {
+  chai
+    .request(server)
+    .post('/login')
+    .send({username: 'pass123', password: 'password'})
+    .end((err, res) => {
+      expect(res).to.have.status(400);
+      done();
+    });
+});
+
+
+//2
+it('positive : /register', done => {
+  chai
+    .request(server)
+    .post('/register')
+    .send({username: 'user123', password: 'password'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
+});
+
+
+it('Negative : /register. Checking invalid name', done => {
+  chai
+    .request(server)
+    .post('/register')
+    .send({username: 'user123', password: 'password'})
+    .end((err, res) => {
+      expect(res).to.have.status(401);
+      expect(res.body.message).to.equals('Invalid input');
+      done();
+    });
+});
+
+
+//3
+it('positive : /friends', done => {
+  chai
+    .request(server)
+    .post('/friends')
+    .send({username: 'john', password: '1234'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
+});
+
+it('Negative : /friends. Checking invalid name', done => {
+  chai
+    .request(server)
+    .post('/friends')
+    .send({username: '@123', password: '9876'})
+    .end((err, res) => {
+      expect(res).to.have.status(404);
+      expect(res.body.message).to.equals('Invalid input');
+      done();
+    });
+});
--- testcase/server.spec.js
+++ testcase/server.spec.js
@@ -34,19 +34,20 @@ it('positive : /login', done => {
     chai
       .request(server)
       .post('/login')
-      .send({username: 'john', password: '1234'})
+      .send({username: 'user123', password: 'pass123'})
       .end((err, res) => {
+        //expect(res.body).to.have.property('accessToken');
         expect(res).to.have.status(200);
         done();
       });
   });
 
-//We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 200 along with a "Invalid input" message.
+//We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 400 along with a "Invalid input" message.
 it('Negative : /login. Checking invalid name', done => {
   chai
     .request(server)
     .post('/login')
-    .send({username: '@123', password: '9876'})
+    .send({username: 'invalidUser', password: 'invalidPassword'})
     .end((err, res) => {
       expect(res).to.have.status(400);
       done();
